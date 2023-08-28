296e7b7c5a682bd24891260451414a9658b58d4d
Paula <mpcastanedagarcia@gmail.com>
2023-08-27 23:46:50 -0500

    mejoras de project-3

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-app.use(express.json());
-
 
 const listViewRouter = require('./list-view-router');
 const listEditRouter = require('./list-edit-router');
 
-app.use('/tasks', listViewRouter);
-app.use('/tasks', listEditRouter);
+// Middleware para validar métodos HTTP válidos
+const validateMethod = (req, res, next) => {
+    if (['GET', 'POST', 'PUT', 'DELETE'].includes(req.method)) {
+        next();
+    } else {
+        res.status(405).send('Método HTTP no permitido');
+    }
+};
 
+app.use(express.json());
+app.use(validateMethod);
+
+app.use('/list-view', listViewRouter);
+app.use('/list-edit', listEditRouter);
 
 app.listen(PORT, () => {
-    console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
