a7499e0a61e0334d44be9386d1af4716e5e206cc
Paula <mpcastanedagarcia@gmail.com>
2023-08-29 21:31:42 -0500

    actualicé proyecto según project-4

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 3000;
 
-const listViewRouter = require('./list-view-router');
-const listEditRouter = require('./list-edit-router');
+// ...
 
-// Middleware para validar métodos HTTP válidos
-const validateMethod = (req, res, next) => {
-    if (['GET', 'POST', 'PUT', 'DELETE'].includes(req.method)) {
-        next();
-    } else {
-        res.status(405).send('Método HTTP no permitido');
+const users = [
+    { username: 'user1', password: 'password1' },
+    { username: 'user2', password: 'password2' }
+];
+
+app.use(bodyParser.json());
+
+// Ruta de autenticación
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (!user) {
+        return res.status(401).json({ error: 'Credenciales inválidas' });
     }
-};
 
-app.use(express.json());
-app.use(validateMethod);
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-app.use('/list-view', listViewRouter);
-app.use('/list-edit', listEditRouter);
-
-app.listen(PORT, () => {
-    console.log(`Servidor iniciado en el puerto ${PORT}`);
+    res.json({ token });
 });
---
+++ auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ error: 'Token no proporcionado' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ error: 'Token inválido' });
+        }
+
+        req.user = decoded;
+        next();
+    });
+};
